1e97a4d216a3b251d17b7bbf3bf89eee72d55002
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-12-13 11:42:27 +0000

    [Add] いいね非同期化

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -1,16 +1,16 @@
 class Public::LikesController < ApplicationController
-  before_action :authenticate_user!
 
   def create
     @post_like = Like.new(end_user_id: current_end_user.id, post_id: params[:post_id])
     @post_like.save
-    redirect_to post_path(params[:post_id])
+    flash[:notice] = '投稿に「いいね！」しました。'
+    @post = Post.find(params[:post_id])
   end
 
   def destroy
     @post_like = Like.find_by(end_user_id: current_end_user.id, post_id: params[:post_id])
     @post_like.destroy
-    redirect_to post_path(params[:post_id])
+    @post = Post.find(params[:post_id])
   end
 
 end
