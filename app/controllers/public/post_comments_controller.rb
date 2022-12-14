9d41b830a760d6591e3d4397bd22bf6b67f188b2
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-12-14 10:11:49 +0000

    [Add] コメント非同期化

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,18 +1,17 @@
 class Public::PostCommentsController < ApplicationController
 
   def create
-    post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     comment = current_end_user.post_comments.new(post_comment_params)
-    comment.post_id = post.id
+    comment.post_id =@post.id
     comment.save
     flash[:notice] = '投稿にコメントしました。'
-    redirect_to post_path(post)
   end
 
   def destroy
     PostComment.find(params[:id]).destroy
-    redirect_to post_path(params[:post_id])
     flash[:notice] = 'コメントを削除しました。'
+    @post = Post.find(params[:post_id])
   end
 
   private
