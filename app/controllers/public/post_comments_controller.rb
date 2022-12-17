19de929847d5692f02ed1027b9e6e54a721d271b
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-12-17 08:50:21 +0000

    [Fix]フラッシュメッセージ

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -4,12 +4,14 @@ class Public::PostCommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     comment = current_end_user.post_comments.new(post_comment_params)
     comment.post_id =@post.id
-    comment.save, notice: '投稿にコメントしました'
+    comment.save
+    flash[:notice] = '投稿にコメントしました'
   end
 
   def destroy
     PostComment.find(params[:id]).destroy
-    @post = Post.find(params[:post_id]), alert: 'コメント削除しました'
+    flash[:alert] = 'コメントを削除しました'
+    @post = Post.find(params[:post_id])
   end
 
   private
