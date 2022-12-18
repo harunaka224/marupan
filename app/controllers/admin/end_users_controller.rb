490f9d6084d34381d08e18765a303097615ee561
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-12-18 09:33:41 +0000

    [Add]管理者機能

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -1,2 +1,28 @@
 class Admin::EndUsersController < ApplicationController
+  #before_action :authenticate_admin!
+
+  def show
+    @end_user = EndUser.find(params[:id])
+  end
+
+  def edit
+    @end_user = EndUser.find(params[:id])
+  end
+
+  def update
+    @end_user = EndUser.find(params[:id])
+    if @end_user.update(end_user_params)
+      redirect_to admin_end_user_path
+    else
+      render "edit"
+    end
+  end
+
+
+  private
+
+  def end_user_params
+    params.require(:end_user).permit(:name, :email, :is_deleted)
+  end
 end
+
