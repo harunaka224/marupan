63f4cb1d63138612a2ba97097e919bfb3af67ad6
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-12-19 02:30:13 +0000

    [Add]いいねランキング機能

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,6 @@
 class Public::HomesController < ApplicationController
 
   def top
+    @all_ranks = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
   end
 end
