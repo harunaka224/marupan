d0ca47c4861aedc1f4d131de0f9f7e8719f93236
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-12-19 03:23:56 +0000

    [Fix]管理者機能

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,5 +1,5 @@
 class Admin::HomesController < ApplicationController
-  #before_action :authenticate_admin!
+  before_action :authenticate_admin!
 
   def top
     @end_users = EndUser.all.page(params[:page]).per(10)
